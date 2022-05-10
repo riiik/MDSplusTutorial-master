c8aeed77b2f2dcf98e92581be2062668c17bc3ce
Richard Padden <richard.padden@ukaea.uk>
2022-05-10 17:40:14 +0000

    add env variable

--- set_env.sh
+++ set_env.sh
@@ -1,2 +1,4 @@
 #! /bin/sh
 export my_tree_path=~/projects/MDSplusTutorial-master/trees/
+export my_subtree_path=~/projects/MDSplusTutorial-master/trees/
+export CLASSPATH=$CLASSPATH:$MDSPLUS_DIR/java/classes/jScope.jar:$MDSPLUS_DIR/java/classes/jTraverser.jar:$MDSPLUS_DIR/java/classes/jDevices.jar:$MDSPLUS_DIR/java/classes/MDSobjects.jar:$MDSPLUS_DIR/java/classes/jDispatcher.jar
