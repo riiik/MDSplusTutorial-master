827333800bee24d7ecff962ad2c7a4b3b4254270
Richard Padden <richard.padden@ukaea.uk>
2022-05-12 16:19:55 +0000

    add env for marte mds tree

--- set_env.sh
+++ set_env.sh
@@ -1,4 +1,8 @@
 #! /bin/sh
+export CLASSPATH=$CLASSPATH:$MDSPLUS_DIR/java/classes/jScope.jar:$MDSPLUS_DIR/java/classes/jTraverser.jar:$MDSPLUS_DIR/java/classes/jDevices.jar:$MDSPLUS_DIR/java/classes/MDSobjects.jar:$MDSPLUS_DIR/java/classes/jDispatcher.jar
+
 export my_tree_path=~/projects/MDSplusTutorial-master/trees/
 export my_subtree_path=~/projects/MDSplusTutorial-master/trees/
-export CLASSPATH=$CLASSPATH:$MDSPLUS_DIR/java/classes/jScope.jar:$MDSPLUS_DIR/java/classes/jTraverser.jar:$MDSPLUS_DIR/java/classes/jDevices.jar:$MDSPLUS_DIR/java/classes/MDSobjects.jar:$MDSPLUS_DIR/java/classes/jDispatcher.jar
+
+export marte_mds_path=~/projects/MDSplusTutorial-master/trees/
+
