bf8da48abc2806c11ec00921cde7ddd955dc9b5a
Richard Padden <richard.padden@ukaea.uk>
2022-05-16 15:02:43 +0100

    add to setup script

--- setenv.sh
+++ setenv.sh
@@ -6,3 +6,12 @@ export my_subtree_path=~/projects/MDSplusTutorial-master/trees/
 
 export marte_mds_path=~/projects/MDSplusTutorial-master/trees/
 
+
+export CONF=$MARTe2_DIR/../MARTe2-demos-sigtools/Configurations
+export BIN=$MARTe2_DIR/../bin
+export TREES=$MARTe2_DIR/../MARTe2-demos-sigtools/Trees
+source $BIN/setenv.sh
+sudo chown -R xtermuser /opt/MARTe2/
+git clone git@git.ccfe.ac.uk:cboswel/MDSTest.git $BIN/MDSTest
+cd $BIN/MDSTest
+source $BIN/MDSTest/install.sh
